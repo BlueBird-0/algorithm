1b2c747605b10510398f548e2512e381cee33cc6
DESKTOP-8CDNODQ\GLIM. 23_29 <ihjeon@iglim.net>
2023-12-24 00:14:22 +0900

    https://www.acmicpc.net/problem/4673  brute force 난이도 쉬움

---
+++ 23년12월/23년12월/12월23일.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+
+const static int LIMIT = 10000;
+
+int d(int n);
+void Q4673()
+{
+	bool result[LIMIT + 1] = { false, };
+	for (int i = 0; i <= LIMIT; i++) {
+		int index = d(i);
+		if (index <= LIMIT)
+		{
+			result[index] = true;
+		}
+	}
+
+	for (int i = 0; i <= LIMIT; i++) {
+		if (result[i] == false)
+			printf("%d\n", i);
+	}
+}
+
+int d(int n)
+{
+	std::vector<int> arr;
+	int position = n;
+	int result = n;
+	while (position > 0)
+	{
+		arr.push_back(position % 10);
+		position /= 10;
+	}
+
+	for (int &i : arr)
+	{
+		result += i;
+	}
+	return result;
+}
---
+++ 23년12월/23년12월/main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+void Q4673();	//¼¿ÇÁ³Ñ¹ö
+
+int main()
+{
+	Q4673();
+	return 0;
+}
