faecb109e91f46f90b0c00356a994597b8806d3f
DESKTOP-BO89M67\ij351 <ij3512@naver.com>
2024-01-01 23:24:21 +0900

    BFS 문제 : 난이도는 쉬움, 문제2번 반복 요구, 풀이속도 오래 걸림.

---
+++ 24년1월/24년1월/1월1일.cpp
@@ -0,0 +1,69 @@
+//https://www.acmicpc.net/problem/1012
+#include <iostream>
+#include <queue>
+using namespace std;
+namespace {
+	int Testcase, M, N, K;
+	bool map[51][51] = { false, };
+	int resultCount = 0;
+
+	queue<pair<int, int>> Q;
+
+	void InsertQ(int x, int y)
+	{
+		if (x < 0 || x >M || y<0 || y>N)
+			return ;
+		else
+			Q.push(make_pair(x, y));
+	}
+	void StartBFS(int x, int y)
+	{
+		resultCount++;
+		Q.push(make_pair(x, y));
+		while (Q.size() != 0)
+		{
+			pair<int, int> position = Q.front();
+			Q.pop();
+
+			if (map[position.second][position.first])
+			{
+				map[position.second][position.first] = false;
+
+				InsertQ(position.first - 1, position.second);
+				InsertQ(position.first + 1, position.second);
+				InsertQ(position.first, position.second - 1);
+				InsertQ(position.first, position.second + 1);
+			}
+		}
+	}
+}
+
+int Q1012()
+{
+	cin >> Testcase;
+
+	while (Testcase--)
+	{
+		cin >> M >> N >> K;
+		resultCount = 0;
+		for (int i = 0; i < K; i++)
+		{
+			int y, x;
+			cin >> x >> y;
+			map[y][x] = true;
+		}
+
+		for (int y = 0; y < N; y++)
+		{
+			for (int x = 0; x < M; x++)
+			{
+				if (map[y][x])
+				{
+					StartBFS(x, y);
+				}
+			}
+		}
+		cout << resultCount << endl;
+	}
+	return 0;
+}
---
+++ 24년1월/24년1월/main.cpp
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+int Q1012();	//1월 1일. 유기농 배추
+
+int main()
+{
+	Q1012();
+	system("pause");
+	return 0;
+}
