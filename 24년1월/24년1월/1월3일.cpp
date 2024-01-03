325fa8e4645fc651c00e4cd666a9876812a43eb0
DESKTOP-8CDNODQ\GLIM. 23_29 <ihjeon@iglim.net>
2024-01-03 10:56:05 +0900

    최소 힙 구현 : std priority_queue 이용해서 구현. cin.tie(0); 없을 경우 시간초과 발생

---
+++ 24년1월/24년1월/1월3일.cpp
@@ -0,0 +1,35 @@
+//https://www.acmicpc.net/problem/11279
+#include <iostream>
+#include <queue>
+using namespace std;
+
+//int Q11279()
+int main()
+{
+	cin.tie(0);
+	priority_queue<int> maxQ;
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		int x;
+		cin >> x;
+		if (x == 0)
+		{
+			if (maxQ.size() == 0)
+			{
+				cout << 0 << '\n';
+			}
+			else {
+				cout << maxQ.top() << '\n';
+				maxQ.pop();
+			}
+		}
+		else
+		{
+			maxQ.push(x);
+		}
+	}
+
+	return 0;
+}
--- 24년1월/24년1월/main.cpp
+++ 24년1월/24년1월/main.cpp
@@ -2,10 +2,11 @@
 #include <iostream>
 int Q1012();	//1월 1일. 유기농 배추
 int Q1927();	//1월 1일_2. 최소 힙
+int Q11279();
 
 int main()
 {
-	Q1927();
+	Q11279();
 	system("pause");
 	return 0;
 }
