5f4d810846f7dd76f446a6aacb76d4801fefa714
DESKTOP-8CDNODQ\GLIM. 23_29 <ihjeon@iglim.net>
2024-02-05 12:39:26 +0900

    수학/기하학 : 난이도 중간. 구현에 오래걸리고 제출 많이 틀림. 수학적 센스를 높여야 할듯

---
+++ 24년1월/24년2월/2월5일.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+namespace {
+	void Swap(double& x1, double &y1, double &r1, double &x2, double &y2, double &r2)
+	{
+		double tempX = x1;
+		double tempY = y1;
+		double tempR = r1;
+		x1 = x2;
+		y1 = y2;
+		r1 = r2;
+		x2 = tempX;
+		y2 = tempY;
+		r2 = tempR;
+	};
+}
+
+int main()
+{
+	int T;
+	cin >> T;
+	while(T--)
+	{
+		double x1, y1, r1, x2, y2, r2;
+		cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
+
+		//더 큰 원이 1번이이 되도록 스왑
+		if (r2 > r1)
+		{
+			Swap(x1, y1, r1, x2, y2, r2);
+		}
+
+		//동일한경우 =-1
+		if (x1 == x2 && y1 == y2) {
+			if (r1 == r2)
+				cout << -1 << '\n';
+			else
+				cout << 0 << '\n';
+		}
+		//r2가 내부에 있는 경우
+		else if (sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2)) < r1)
+		{
+			//  완전 내접할때 = 1
+			if (sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2)) + r2 == r1)
+				cout << 1 << '\n';
+			//  내포할때 = 0
+			else if (sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2)) + r2 < r1)
+				cout << 0 << '\n';
+			//  그게 아니면 = 2
+			else
+				cout << 2 << '\n';
+		}
+		//r2가 외부에 있는 경우
+		else if (sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2)) > r1)
+		{
+			//  완전 외접할때 = 1
+			if (sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2)) - r2 == r1)
+				cout << 1 << '\n';
+			//  외부에 있을때 = 0
+			else if (sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2)) - r2 > r1)
+				cout << 0 << '\n';
+			//  그게 아니면 = 2
+			else
+				cout << 2 << '\n';
+		}
+		//r2가 r1 선 위에 있는 경우
+		else if (sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2)) == r1)
+		{
+			cout << 2 << '\n';
+		}
+		
+
+		
+
+	}
+	return 0;
+}
--- 24년1월/24년2월/main.cpp
+++ 24년1월/24년2월/main.cpp
@@ -1,11 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 
-int Q11438();	//LCA2. 
+int Q11438();	//2월 1일. LCA2
+int Q1002();	//2월 5일. 터렛
 
 int main()
 {
-	Q11438();
+	Q1002();
 	system("pause");
 	return 0;
 }
