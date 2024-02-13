04d0b5b380c27e7323c23603b01d44e2af02f0f4
DESKTOP-8CDNODQ\GLIM. 23_29 <ihjeon@iglim.net>
2024-02-13 17:28:58 +0900

    Bruteforce/ 수학적 사고 : 난이도 어려움. Bfuteforce를 떠올리기가 어려움. 다시 구현해 볼 것

---
+++ 24년1월/24년2월/2월13일.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+namespace {
+	int T, N;
+	int Arr[20];
+	pair<int, int> points[20];
+	int x, y;
+	double ans;
+
+	double xSum, ySum, xNegSum, yNegSum;
+
+	double VectorSum(double x, double y)	//x2-x1, y2-y1이 올때, 벡터합 반환
+	{
+		return sqrt(x * x + y * y);
+	}
+
+	void BruteForce(int n, int k)
+	{
+		if (k == N / 2)
+		{
+			xSum = 0;
+			ySum = 0;
+			xNegSum = 0;
+			yNegSum = 0;
+			for (int i = 0; i < N; i++)
+			{
+				if (Arr[i] == 0) 
+				{
+					xSum += points[i].first;
+					ySum += points[i].second;
+				}
+				else {
+					xNegSum += points[i].first;
+					yNegSum += points[i].second;
+				}
+			}
+			if (ans == -1)
+			{
+				ans = VectorSum(xSum - xNegSum, ySum - yNegSum);
+			}
+			else {
+				ans = min(ans, VectorSum(xSum - xNegSum, ySum - yNegSum));
+			}
+			return;
+		}
+		else if (n == N){
+			return;
+		}
+		else {
+			for (int i = 0; i < 2; i++)
+			{
+				if (i == 0)
+					BruteForce(n + 1, k);
+				else {
+					Arr[n] = 1;
+					BruteForce(n + 1, k + 1);
+					Arr[n] = 0;
+				}
+			}
+		}
+	}
+}
+
+int Q1007()
+{
+	ios::sync_with_stdio(false);
+	cout.tie(0);
+	cout.precision(12);
+	cout << fixed;
+	cin >> T;
+	while (T--)
+	{
+		cin >> N;
+		ans = -1;
+		for (int j = 0; j < N; j++)
+		{
+			Arr[j] = 0;
+		}
+
+
+		for (int i = 0; i < N; i++)
+		{
+			cin >> x >> y;
+			points[i] = make_pair(x, y);
+		}
+		BruteForce(0, 0);
+		cout << ans << '\n';
+	}
+
+	return 0;
+}
---
+++ 24년1월/24년2월/2월8일.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+namespace {
+	int N, W;
+	int map[2][10000];
+
+	bool CheckCombine(int a, int b)
+	{
+		return a + b <= W ? true : false;
+	}
+
+	int DP(int n)
+	{
+		if (n == 0)
+		{
+			return 0;
+		}
+		if (n == 1)
+		{
+			return (map[0][0] + map[1][0] <= W) ? 1 : 2;
+		}
+		else if (n == 2)
+		{
+			int front = 0;
+
+
+
+
+		}
+
+		min(DP(n-2), DP(n-1));
+		return 0;
+	}
+}
+
+
+int Q1006() {
+	int T;
+	cin >> T;
+	while (T--)
+	{
+		cin >> N >> W;
+		for (int i = 0; i < N; i++)
+		{
+			cin >> map[0][i]; 
+		}
+		for (int i = 0; i < N; i++)
+		{
+			cin >> map[1][i];
+		}
+	}
+
+	//n == 10이라면?
+	DP(10);
+
+	return 0;
+}
+
