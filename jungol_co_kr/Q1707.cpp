6963c785c40f513db66e243f34c93e682cba28a3
inhakJeon <ij3512@naver.com>
2024-07-24 13:19:04 +0900

    2중for문 : 달팽이 출력문제. 구현에 생각보다 시간 오래걸림

---
+++ jungol_co_kr/Q1707.cpp
@@ -0,0 +1,106 @@
+#include <stdio.h>
+int arr[100][100] = {0,};	
+int n;
+int x=0, y=0;
+int num=1;
+
+void fillRight()
+{
+	while(1)
+	{
+		if(x+1 >= n || arr[y][x+1] != 0)
+			break;
+		x++;
+		arr[y][x] = num++;
+	}
+	return;
+}
+
+void fillLeft()
+{
+	while(1)
+	{
+		if(x-1 < 0 || arr[y][x-1] != 0)
+			break;
+		x--;
+		arr[y][x] = num++;
+	}
+	return;
+}
+
+void fillUp()
+{
+	while(1)
+	{
+		if(y-1 < 0 || arr[y-1][x] != 0)
+			break;
+		y--;
+		arr[y][x] = num++;
+	}
+	return;
+}
+
+void fillDown()
+{
+	while(1)
+	{
+		if(y+1 >= n || arr[y+1][x] != 0)
+			break;
+		y++;
+		arr[y][x] = num++;
+	}
+	return;
+}
+
+int checkEnd(){
+	if((x+1 >= n || arr[y][x+1] != 0) &&
+		(x-1 < 0 || arr[y][x-1] != 0) &&
+		(y-1 < 0 || arr[y-1][x] != 0) &&
+		(y+1 >= n || arr[y+1][x] != 0))
+		return 1;
+	return 0;
+}
+
+int main()
+{
+	scanf("%d",&n);
+	arr[y][x] = num++;
+	int dir = 0; // 0R 1D 2L 3U
+	while(1)
+	{
+		if(dir == 0)
+		{
+			fillRight();
+			dir=1;
+		}
+		else if(dir == 1)
+		{
+			fillDown();
+			dir=2;
+		}
+		else if(dir == 2)
+		{
+			fillLeft();
+			dir=3;
+		}
+		else
+		{
+			fillUp();
+			dir=0;
+		}
+		if(checkEnd())
+			break;
+	}
+	
+	//Ãâ·ÂºÎ
+	for(int y=0;y<n; y++)
+	{
+		for(int x=0; x<n; x++)
+		{
+			printf("%d ", arr[y][x]); 
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
