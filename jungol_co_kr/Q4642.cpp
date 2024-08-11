6ece254daf073d1cb99d41cbc490c93f86ebbedd
inhakJeon <ij3512@naver.com>
2024-08-11 16:21:20 +0900

    STL연습 : 연산자 오버로딩을 통한 클래스 비교 연습2.

---
+++ jungol_co_kr/Q4642.cpp
@@ -0,0 +1,34 @@
+﻿#include <cstdio>
+#include <string>
+#include <algorithm>
+
+struct Student {
+	char name[20];
+	int score;
+	bool operator<(const Student& r) const {
+		if (score == r.score) {
+			if (strcmp(name, r.name) > 0)
+				return true;
+			return false;
+		}
+		return score < r.score;
+	}
+};
+
+using namespace std;
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	Student* s = (Student*)malloc(sizeof(Student) * 50001);
+	for (int i = 0; i < n; i++) {
+		scanf("%s %d", s[i].name, &s[i].score);
+	}
+
+	sort(s + 0, s + n);
+
+	reverse(s + 0, s + n);
+	for (int i = 0; i < n; i++)
+		printf("%s %d\n", s[i].name, s[i].score);
+	free(s);
+}
