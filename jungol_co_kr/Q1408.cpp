4271a29fc47c9a33e39d691b10f33eff662b27b0
inhakJeon <ij3512@naver.com>
2024-09-27 13:37:00 +0900

    DP : LIS (Longest Increase Subsequence) 응용. 문제보고 LIS가 바로 떠오르지 않음. 2차원 행렬로 풀어보려 했으나 테스트케이스 실패

---
+++ jungol_co_kr/Q1408.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int N;
+vector<pair<int, int>> arr;
+int dp[501] = { 0, };
+
+int main()
+{
+	int src, dst, yMax = 0, xMax = 0;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		scanf("%d %d", &src, &dst);
+		arr.push_back(make_pair(src, dst));
+	}
+	sort(arr.begin(), arr.end());
+	//LIS (최장 증가 부분 수열)
+	int LIS = 0;
+	dp[0] = 1;
+	for (int i = 0; i < arr.size(); i++) {
+		int m = 0;
+		for (int j = 0; j < i; j++) {
+			if(arr[i].second > arr[j].second)
+				m = max(dp[j], m);
+		}
+		dp[i] = m + 1;
+		LIS = max(dp[i], LIS);
+	}
+
+	printf("%d", arr.size()-LIS);
+
+	return 0;
+}
