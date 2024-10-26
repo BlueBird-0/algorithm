5bbea3b7de196818d51c226cea9a1017947a6638
inhakJeon <ij3512@naver.com>
2024-10-26 23:59:16 +0900

    DP : DP 색종이 쌓기 응용 문제. 난이도 어려움(미해결)
    문제 풀때 조건(색좋이 쌓을때) 잘 보고 문제풀이 시작할 것.
    DP는 조금잘못되면 아예 다른값이 나옴. 디버그 할때 print로 찍어보며 할것

---
+++ jungol_co_kr/Q1510.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std; 
+
+int n;
+
+int main()
+{
+	cin >> n; 
+	vector<pair<int, int>> arr;
+	int a, b;
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", &a, &b);
+		if (a>b)
+			arr.push_back(make_pair(a, b));
+		else
+			arr.push_back(make_pair(b, a));
+		
+	}
+	//면적기준 정렬
+	sort(arr.begin(), arr.end(), [](pair<int, int>& a, pair<int, int>& b)->bool {return a.first * a.second < b.first* b.second; });
+
+
+	int stack[100] = {0,};
+
+	for(int i=0; i<n; i++)
+	{
+		for (int j = i - 1; j >= 0; j--) {
+			if (arr[i].first >= arr[j].first && arr[i].first >= arr[j].second) {
+				stack[i] = max(stack[j] + 1, stack[i]);
+			}
+		}
+	}
+	printf("%d", stack[n-1]);
+}
