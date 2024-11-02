4e53a224dfd0f03fe3b573e7dd4e28d0bcfdef6f
inhakJeon <ij3512@naver.com>
2024-11-02 23:00:10 +0900

    Greedy: 숫자 더하기 응용 문제. (재풀이)
    1씩 증가하며 만들수 있는숫자인지 체크하는 알고리즘 (어려움).

--- jungol_co_kr/Q2499.cpp
+++ jungol_co_kr/Q2499.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
 #include <algorithm>
 using namespace std;
+int N;
+int arr[1000];
 
 int main()
 {
-    int n;
-    int arr[1000];
 
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)
+		scanf("%d", &arr[i]);
 
-    sort(arr, arr + n, less<int>());
+	sort(&arr[0], &arr[N]);
 
-    int res = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if (res + 1 < arr[i])
-            break;
-        res += arr[i];
-    }
+	int res = 0;
+	for (int i = 0; i < N; i++)
+	{
+		if (res + 1 < arr[i])
+			break;
+		res += arr[i];
+	}
 	res += 1;
 
-    printf("%d\n", res);
+	printf("%d\n", res);
 
-    return 0;
+	return 0;
 }
