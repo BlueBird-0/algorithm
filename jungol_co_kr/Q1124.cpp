d2c40c257ea2a51bcb4fb3ba35e760752ac9b8ce
inhakJeon <ij3512@naver.com>
2024-11-03 22:42:59 +0900

    자료구조 : 다각형에서 최대 직사각형 크기 구하기(재풀이)
    100*100 정도는 2중for문으로 전수조사 하는게 해결책일 수 있음.
    특별한 알고리즘이 있을지 문제의 크기를 보고 고민할 것.

--- jungol_co_kr/Q1124.cpp
+++ jungol_co_kr/Q1124.cpp
@@ -1,89 +1,48 @@
-#include <stdio.h>
-
-int squ[100][100] = { 0 }; //도화지
-int dot[100][2] = { 0 }; //색종이를 붙인 위치
-//[0]=x좌표 [1]=y좌표
-
-void fill(int x, int y); //색종이가 있는 영역 표시
-int search(void); //직사각형이 만들어질 수 있는 경우의 수
-int cal(int x1, int x2, int y1, int y2); //직사각형의 넓이 계산
-int main(void)
-{
-    int n, i, result;
-
-    scanf(" %d", &n); //색종이의 수
-
-    for (i = 0; i < n; i++)
-    {
-        scanf(" %d %d", &dot[i][0], &dot[i][1]); //색종이를 붙인 위치
-        fill(dot[i][0], dot[i][1]); //색종이가 있는 영역 표시
-    }
-
-    result = search();
-
-    printf("%d", result);
-
-    return 0;
+#include <iostream>
+using namespace std;
+bool map[100][100] = { 0, };
+int N;
+void fill(int sttx, int stty) {
+	for (int y = 0; y < 10; y++) {
+		for (int x = 0; x < 10; x++) {
+			map[stty + y][sttx + x] = true;
+		}
+	}
 }
-void fill(int x, int y) //색종이가 있는 영역 표시
-{
-    int i, j;
 
-    for (i = x; i < (x + 10); i++)
-    {
-        for (j = y; j < (y + 10); j++)
-            squ[i][j] = 1; //1로 채우기
-    }
+int cal(int x, int y, int xx, int yy) {
+
+	for (int i = x; i <= xx; i++) {
+		for (int j = y; j <= yy; j++) {
+			if (!map[j][i])
+				return 0;
+		}
+	}
+	return (xx - x + 1) * (yy - y + 1);
 }
-int search(void) //직사각형이 만들어질 수 있는 경우의 수
+
+int main()
 {
-    int x1, x2, y1, y2, max = 100, sum;
+	cin >> N;
+	int x, y;
+	for (int i = 0; i < N; i++) {
+		scanf("%d %d", &x, &y);
+		fill(x, y);
+	}
 
-    for (x1 = 0; x1 < 100; x1++)
-    {
-        for (y1 = 0; y1 < 100; y1++)
-        {
-            if (squ[x1][y1] == 1) //사각형의 시작점을 찾았을 때
-            {
-                for (x2 = x1; x2 < 100; x2++)
-                {
-                    for (y2 = y1; y2 < 100; y2++)
-                    {
-                        if (squ[x2][y2] == 1)
-                        {
-                            //x1부터 x2까지 y1부터 y2까지 모두 1이라고 가정한 넓이가
-                            //현재 최대값보다 작거나 같을 경우
-                            //cal 함수를 실행하지 않아도 된다. 
-                            if ((x2 - x1 + 1) * (y2 - y1 + 1) <= max)
-                                continue;
+	int result = 0;
+	for (int y = 0; y < 100; y++) {
+		for (int x = 0; x < 100; x++) {
 
-                            sum = cal(x1, x2, y1, y2);
+			for (int yy = y; yy < 100; yy++) {
+				for (int xx = x; xx < 100; xx++) {
+					if (result >= (xx - x + 1) * (yy - y + 1))
+						continue;
 
-                            if (sum > max)
-                                max = sum;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    return max;
-}
-int cal(int x1, int x2, int y1, int y2) //직사각형의 넓이 계산
-{
-    int i, j, sum = 0;
-
-    for (i = x1; i <= x2; i++)
-    {
-        for (j = y1; j <= y2; j++)
-        {
-            if (squ[i][j] == 0) //사각형이 끊기면 0반홤
-                return 0;
-
-            sum++; //사각형의 넓이
-        }
-    }
-
-    return sum;
+					result = max(result, cal(x, y, xx, yy));
+				}
+			}
+		}
+	}
+	printf("%d", result);
 }
