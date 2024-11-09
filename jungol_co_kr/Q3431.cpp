695c0ab25ac35feb8f4e83ca50c324f83045caa7
inhakJeon <ij3512@naver.com>
2024-11-09 23:55:27 +0900

    greedy : 통나무로 갈수있는길 찾기. (재풀이)
    탐욕 알고리즘 특징 이해하기. 문제에서 출력으로 요구하는게 어떤건지 확인 할 것.

--- jungol_co_kr/Q3431.cpp
+++ jungol_co_kr/Q3431.cpp
@@ -2,67 +2,53 @@
 #include <vector>
 #include <memory.h>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
-int N, Q;
-
-class TREE{
-public:
-	int stt;
-	int end;
-	vector<int> link;
-};
-
-TREE arr[100001];
-
+vector<pair<int, int>> arr(100000);	// x1, x2
+vector<vector<int>> link(100000);
 int main()
 {
+	int N, Q;
 	cin >> N >> Q;
 	int y;
-	TREE temp;
-	for (int i = 1; i <= N; i++) {
-		scanf("%d %d %d", &temp.stt, &temp.end, &y);
-		arr[i] = temp;
-
-		for (int j = 1; j < i; j++) {
-			if ((arr[j].stt <= arr[i].stt && arr[j].end >= arr[i].stt) || (arr[j].stt <= arr[i].end && arr[j].end >= arr[i].end) ||
-				(arr[i].stt <= arr[j].stt && arr[i].end >= arr[j].stt) || (arr[i].stt <= arr[j].end && arr[i].end >= arr[j].end)) {
-				arr[j].link.push_back(i);
-				arr[i].link.push_back(j);
-			}
-		}
+	for (int i = 0; i < N; i++) {
+		scanf("%d %d %d", &arr[i].first, &arr[i].second, &y);
 	}
+	sort(&arr[0], &arr[N]);
+	
+	int minV, maxV=0;
+	int a = 0;
+	for (int i = 0; i < N; i++) {
+		if (arr[i].first <= maxV)
+			continue;
 
-	//Ã£±â
-	int q1, q2;
-	bool visit[100001];
-	for (int i = 0; i < Q; i++) {
-		memset(visit, 0, sizeof(visit));
-		scanf("%d %d", &q1, &q2);
+		minV = arr[i].first;
+		maxV = max(arr[i].second, maxV);
 
-		queue<int> links;
-		for (auto e : arr[q1].link) {
-			links.push(e);
-		}
-
-		bool arrive = false;
-		while (!links.empty()) {
-			int t = links.front();
-			links.pop();
-			visit[t] = true;
-
-			if (t == q2) {
-				arrive = true;
+		for (int j = i + 1; j < N; j++) {
+			if (arr[j].first <= maxV) {
+				link[a].push_back(j);
+				maxV = max(arr[j].second, maxV);
+			}
+			else {
 				break;
 			}
-
-			for (auto e : arr[t].link) {
-				if (!visit[e]) {
-					links.push(e);
-				}
-			}
 		}
-		printf("%d\n", arrive? 1 : 0);
+
+		a++;
+	}
+
+	int stt, end;
+	for (int i = 0; i < Q; i++) {
+		scanf("%d %d", &stt, &end);
+	}
+
+	for (auto e : link) {
+		for (auto f : e) {
+			printf("%3d", f);
+		}
+		printf("\n");
 	}
 }
