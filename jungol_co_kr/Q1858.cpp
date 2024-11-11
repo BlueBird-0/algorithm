b38cf38372ee290fbb3d210ae4fb40c6e24ea06e
inhakJeon <ij3512@naver.com>
2024-11-11 23:38:00 +0900

    LCS응용 : DNA유사도. (재풀이) 난이도 매우어려움.
    LCS작성시 증감값 확인할 것.
    나중에 다시 풀어볼 것.

--- jungol_co_kr/Q1858.cpp
+++ jungol_co_kr/Q1858.cpp
@@ -1,36 +1,72 @@
 #include <iostream>
+#include <vector>
 #include <string>
+#include <algorithm>
+#include <climits>
+
 using namespace std;
-string strX, strY;
-int LCS[1001][1001] = { 0, };
-string resultX, resultY;
-int resultV = 0;
 
-int main()
-{
-	int lenX, lenY;
-	cin >> lenX >> strX >> lenY >> strY;
-	for (int y = 1; y <= lenY; y++) {
-		for (int x = 1; x <= lenX; x++) {
-			if (strY[y-1] == strX[x-1]) {
-				LCS[y][x] = LCS[y - 1][x - 1] + 1;
+int main() {
+    int la, lb;
+    string a, b;
+    cin >> la >> a >> lb >> b;
 
-				int lcs = LCS[y][x];
-				int newValue = lcs * 3 + ((x - lcs) * -2) + ((y - lcs) * -2);
-				if (resultV <= newValue) {
-					resultV = newValue;
-					resultX = strX.substr(0, x);
-					resultY = strY.substr(0, y);
-				}
+    // dp 배열 초기화
+    vector<vector<int>> dp(la + 1, vector<int>(lb + 1, 0)); //null값 추가
 
-			}
-			else {
-				LCS[y][x] = max(LCS[y - 1][x], LCS[y][x - 1]);
-			}
+    // LCS 유사도 계산
+    for (int y = 1; y <= la; y++) {
+        for (int x = 1; x <= lb; x++) {
+            if (a[y - 1] == b[x - 1]) {
+                dp[y][x] = dp[y - 1][x - 1] + 3;
+            }
+            else {
+                dp[y][x] = max({ dp[y - 1][x - 1] - 2, dp[y - 1][x] - 2, dp[y][x - 1] - 2, 0 });
+            }
+        }
+    }
 
-		}
-	}
-	printf("%d\n", resultV);
-	printf("%s\n", resultX.c_str());
-	printf("%s\n", resultY.c_str());
+    // 최대 유사도 및 최대 구간의 끝점 찾기
+    int max_score = INT_MIN;
+    int x = 0, y = 0;
+
+    for (int i = 1; i <= la; i++) {
+        for (int j = 1; j <= lb; j++) {
+            if (dp[i][j] > max_score) {
+                max_score = dp[i][j];
+                y = i;
+                x = j;
+            }
+        }
+    }
+
+    // 최대 구간의 시작점 찾기
+    int inity = y;
+    int initx = x;
+
+    while (dp[y][x] != 0) {
+        if (dp[y - 1][x] == dp[y][x] + 2) {
+            y--;
+        }
+        else if (dp[y][x - 1] == dp[y][x] + 2) {
+            x--;
+        }
+        else {
+            x--;
+            y--;
+        }
+    }
+
+    // 출력
+    cout << max_score << endl;
+    for (int i = y + 1; i <= inity; i++) {
+        cout << a[i - 1];
+    }
+    cout << endl;
+    for (int i = x + 1; i <= initx; i++) {
+        cout << b[i - 1];
+    }
+    cout << endl;
+
+    return 0;
 }
