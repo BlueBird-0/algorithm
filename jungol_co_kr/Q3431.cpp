1fade5a4cf56da9e4392af41187ffda4818e2053
inhakJeon <ij3512@naver.com>
2025-01-03 23:38:46 +0900

    Greedy: 개구리 점프문제 다시풀기. map이용하여 그리디문제 구현.

--- jungol_co_kr/Q3431.cpp
+++ jungol_co_kr/Q3431.cpp
@@ -3,34 +3,46 @@
 #include <memory.h>
 #include <queue>
 #include <algorithm>
+#include <map>
 
 using namespace std;
+struct TREE {
+	int s, e, v;
+};
 
-vector<pair<int, int>> arr(100000);	// x1, x2
-vector<vector<int>> link(100000);
+vector<TREE> arr(100000);	// x1, x2
+vector<map<int, bool>> link;
 int main()
 {
 	int N, Q;
 	cin >> N >> Q;
 	int y;
 	for (int i = 0; i < N; i++) {
-		scanf("%d %d %d", &arr[i].first, &arr[i].second, &y);
+		scanf("%d %d %d", &arr[i].s, &arr[i].e, &y);
+		arr[i].v = i + 1;
 	}
-	sort(&arr[0], &arr[N]);
+	sort(&arr[0], &arr[N], [](TREE &a, TREE &b)->int{
+		if (a.s == b.s) {
+			return a.e < b.e;
+		}
+		return a.s < b.s;
+	});
 	
 	int minV, maxV=0;
 	int a = 0;
 	for (int i = 0; i < N; i++) {
-		if (arr[i].first <= maxV)
+		if (arr[i].s <= maxV)
 			continue;
 
-		minV = arr[i].first;
-		maxV = max(arr[i].second, maxV);
+		minV = arr[i].s;
+		maxV = max(arr[i].e, maxV);
+		link.push_back(map<int, bool>());
+		link[a][arr[i].v] = true;
 
 		for (int j = i + 1; j < N; j++) {
-			if (arr[j].first <= maxV) {
-				link[a].push_back(j);
-				maxV = max(arr[j].second, maxV);
+			if (arr[j].s <= maxV) {
+				link[a][arr[j].v] = true;
+				maxV = max(arr[j].e, maxV);
 			}
 			else {
 				break;
@@ -43,12 +55,21 @@ int main()
 	int stt, end;
 	for (int i = 0; i < Q; i++) {
 		scanf("%d %d", &stt, &end);
+
+		bool able = false;
+		for (auto e : link) {
+			if (e[stt] && e[end]) {
+				able = true;
+				break;
+			}
+		}
+		printf("%d\n", able ? 1 : 0);
 	}
 
-	for (auto e : link) {
+	/*for (auto e : link) {
 		for (auto f : e) {
 			printf("%3d", f);
 		}
 		printf("\n");
-	}
+	}*/
 }
