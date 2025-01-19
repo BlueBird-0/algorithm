18ac85f5564dd2b3dbed36fadf25d5c699f7e0f7
inhakJeon <ij3512@naver.com>
2025-01-19 23:59:27 +0900

    Queue구현: 골드 4. 지렁이게임 구현.
    문제에서 행렬 구할때 x/y 좌표 검토할 할 것.
    방향전환 구현시 쉽게 구현하는법 고려할 것.

---
+++ backjoon_(acmicpc_net)/Q3190.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+const int WALL = 1;
+const int APPLE = 2;
+const int PLAYER = 3;
+int map[101][101] = { 0, };
+struct character {
+	queue<pair<int, int>> pos;	// y, x
+	int dir = 0; // 0=RIGHT, 1=DOWN, 2=LEFT, 3=UP
+	void move() {
+		if (dir == 0)
+			pos.push(make_pair(pos.back().first, pos.back().second + 1));
+		else if(dir == 1)
+			pos.push(make_pair(pos.back().first + 1, pos.back().second));
+		else if(dir == 2)
+			pos.push(make_pair(pos.back().first, pos.back().second - 1));
+		else if(dir == 3)
+			pos.push(make_pair(pos.back().first - 1, pos.back().second));
+	}
+	void turnLeft() {
+		dir += 4;
+		dir--;
+		dir %= 4;
+	}
+	void turnRight() {
+		dir++;
+		dir %= 4;
+	}
+};
+
+int main()
+{
+	int n, k;
+	scanf("%d %d", &n, &k);
+	
+	for (int i = 0; i < n; i++) {
+		map[i][0] = WALL;
+		map[0][i] = WALL;
+		map[i][n + 1] = WALL;
+		map[n + 1][i] = WALL;
+	}
+
+	for (int i = 0; i < k; i++) {
+		int y, x;
+		scanf("%d %d", &x, &y);
+		map[y][x] = APPLE;
+	}
+	queue<pair<int, char>> commands;
+	int cmdCount;
+	scanf("%d", &cmdCount);
+	for (int i = 0; i < cmdCount; i++) {
+		int time;
+		char ch;
+		scanf("%d %c", &time, &ch);
+		commands.push(make_pair(time, ch));
+	}
+	
+	//PLAY
+	character player;
+	player.pos.push(make_pair(1, 1));
+
+	int time = 0;
+	while (1) {
+		time++;
+		//Move
+		player.move();
+
+		if (map[player.pos.back().first][player.pos.back().second] == PLAYER || map[player.pos.back().first][player.pos.back().second] == WALL) {
+			break;
+		}
+		else if (map[player.pos.back().first][player.pos.back().second] == APPLE) {
+			map[player.pos.back().first][player.pos.back().second] = PLAYER;
+		}
+		else {
+			map[player.pos.back().first][player.pos.back().second] = PLAYER;
+			map[player.pos.front().first][player.pos.front().second] = 0;
+			player.pos.pop();
+		}
+		//change direct
+		if (!commands.empty() && commands.front().first == time) {
+			if (commands.front().second == 'L') {
+				player.turnLeft();
+			}
+			else if (commands.front().second == 'D') {
+				player.turnRight();
+			}
+			commands.pop();
+		}
+	}
+	printf("%d", time);
+
+	return 0;
+}
